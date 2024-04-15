e4635ea8fc1ced2527406b1bf8693b2164ef3037
Pawel Zelmanski <pzelmanski1@gmail.com>
2024-04-15 20:37:48 +0200

    wording of comments

--- FsCheck3PBTests/SimpleTypes/SimpleTypesGenerators.cs
+++ FsCheck3PBTests/SimpleTypes/SimpleTypesGenerators.cs
@@ -5,11 +5,11 @@ namespace FsCheck3PBTests.SimpleTypes;
 
 public static class SimpleTypesGenerators
 {
-    // Generator, used to actually generate the values
+    // Generator, used to generate the values
     public static Gen<PositiveInt> GetPositiveInt() =>
         ArbMap.Default.GeneratorFor<int>().Where(x => x >= 0).Select(x => new PositiveInt(x));
     
-    // In order to use the generator in the property-based tests, we need to create an Arbitrary instance
+    // In order to use generator in the property-based tests, we need to create an Arbitrary instance
     // This is a function used to generate PositiveInt parameters of PositiveInt_ShouldBePositive test
     public static Arbitrary<PositiveInt> PositiveIntArb() =>
         Arb.From(GetPositiveInt());
@@ -19,7 +19,7 @@ public static class SimpleTypesGenerators
     public static Arbitrary<int> OverrideIntArb() =>
         ArbMap.Default.GeneratorFor<int>().Where(x => x >= 0).ToArbitrary();
 
-    // When there are two generators generating the same type, the one being higher is used.
+    // When there are two generators generating the same type, the one being higher inside a class is used.
     // This means that the generator below is ignored and `OverrideIntArb` from above is used.
     public static Arbitrary<int> AnotherOverrideIntArb()
     {
